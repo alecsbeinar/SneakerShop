e21a878a2cc52496fd950977d626dc9a48f8abe7
Александр Бейнар <alecsanderbejnar@gmail.com>
2023-11-14 00:29:51 +0300

    Add tests for api

--- SneakerShop/backend/backend/settings.py
+++ SneakerShop/backend/backend/settings.py
@@ -130,6 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES':
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
+    ),
 }
