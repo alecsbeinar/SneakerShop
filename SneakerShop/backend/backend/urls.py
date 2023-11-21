d4193e168861b06983ed3ad39d8fc89df7d7863d
Александр Бейнар <alecsanderbejnar@gmail.com>
2023-11-21 16:53:48 +0300

    Configure urls in correct order

--- SneakerShop/backend/backend/urls.py
+++ SneakerShop/backend/backend/urls.py
@@ -22,14 +22,12 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('', include('shop.urls', namespace='shop')),
-    path('api/', include('shop_api.urls', namespace='shop_api')),
-    path('api/user/', include('users.urls', namespace='users')),
-
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
     path('admin/', admin.site.urls),
+    path('api/', include('shop_api.urls', namespace='shop_api')),
+    path('api/user/', include('users.urls', namespace='users')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    path('', include('shop.urls', namespace='shop')),
 ]
