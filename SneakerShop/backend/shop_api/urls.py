bc4054e6ac702819f653a0e1cb7a8750030e638c
Александр Бейнар <alecsanderbejnar@gmail.com>
2023-11-22 17:16:44 +0300

    Search endpoint

--- SneakerShop/backend/shop_api/urls.py
+++ SneakerShop/backend/shop_api/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from shop_api.views import ProductModelViewSet
+from shop_api.views import ProductModelViewSet, ProductSearch
 
 app_name = 'shop_api'
 router = DefaultRouter()
 router.register('', ProductModelViewSet, basename='product')
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('search/', ProductSearch.as_view(), name='productsearch'),
+    path('', include(router.urls)),
+]
--- SneakerShop/backend/shop_api/views.py
+++ SneakerShop/backend/shop_api/views.py
@@ -2,10 +2,13 @@ from rest_framework import generics, permissions
 from rest_framework import viewsets
 from rest_framework.decorators import permission_classes, action
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from shop.models import Product
 from .serializers import ProductSerializer
+from rest_framework.decorators import authentication_classes, permission_classes
+
 
 
 class UserPermission(permissions.BasePermission):
@@ -30,7 +33,22 @@ class ProductModelViewSet(viewsets.ModelViewSet):
         return Product.objects.all()
 
     def get_object(self, queryset=None, **kwargs):
-        item = self.kwargs.get('pk')
-        obj = get_object_or_404(Product, slug=item)
+        pk = self.kwargs.get('pk')
+        obj = get_object_or_404(Product, slug=pk)
         self.check_object_permissions(self.request, obj)
         return obj
+
+
+class AllowAll(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return True
+
+
+@authentication_classes([])
+@permission_classes([])
+class ProductSearch(generics.ListAPIView):
+    permission_classes = [AllowAll]
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['^name']
